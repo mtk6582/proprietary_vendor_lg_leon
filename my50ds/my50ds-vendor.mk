fd8b958ff02fade7e5efc74be8fe9d03027e5b74
mtk6582 <kolya.musyrmy@yandex.ru>
2017-07-17 10:09:44 +0300

    Initial commit

---
+++ my50ds/my50ds-vendor.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := vendor/lge/ms50ds
+PRODUCT_COPY_FILES += \
+$(LOCAL_PATH)/system/lib/libril.so:system/lib/libril.so \
+$(LOCAL_PATH)/system/lib/gralloc.mt6582.so:system/lib/gralloc.mt6582.so \
+$(LOCAL_PATH)/system/lib/hwcomposer.mt6582.so:system/lib/hwcomposer.mt6582.so \
+$(LOCAL_PATH)/system/lib/libEGL.so:system/lib/libEGL.so \
+$(LOCAL_PATH)/system/lib/libEGL_mali.so:system/lib/libEGL_mali.so \
+$(LOCAL_PATH)/system/lib/libion.so:system/lib/libion.so \
+$(LOCAL_PATH)/system/lib/libMali.so:system/lib/libMali.so \
+$(LOCAL_PATH)/system/lib/libriltmk.so:system/lib/libriltmk.so \
+$(LOCAL_PATH)/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+$(LOCAL_PATH)/system/lib/egl/libGLESv1_mali.so:system/lib/egl/libGLESv1_mali.so \
+$(LOCAL_PATH)/system/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
+$(LOCAL_PATH)/system/lib/hw/audio.primary.mt6582.so:system/lib/hw/audio.primary.mt6582.so \
+$(LOCAL_PATH)/system/lib/hw/audio_policy.deafult.so:system/lib/hw/audio_policy.deafult.so \
+$(LOCAL_PATH)/system/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+$(LOCAL_PATH)/system/usr/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
+$(LOCAL_PATH)/system/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+$(LOCAL_PATH)/system/usr/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+$(LOCAL_PATH)/system/usr/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
